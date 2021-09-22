d57c4d995b4af1a6bdbab58a116c40864e059348
Griefman <stanislavparshin@yandex.ru>
2021-09-23 00:47:49 +0300

    request getMe created

--- 03-lesson_request-to-TelegramAPI/index.php
+++ 03-lesson_request-to-TelegramAPI/index.php
@@ -1,1 +1,18 @@
 <?php
+
+const TOKEN = '2012660956:AAFsAOCgvirTWiF7hc7XdT-DB0WGqcUPjng';
+
+const BASE_URL = 'https://api.telegram.org/bot' . TOKEN . '/';
+
+$url = BASE_URL . 'getMe';
+
+$res = json_decode(file_get_contents($url), 1);
+echo $res->ok;
+debug($res);
+
+
+
+function debug($data)
+{
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+}
